31cad2ef79c66befc32aec2b95e5c92f24dec6c5
oliver <oliverxiaozhen@outlook.com>
2025-10-11 23:29:15 +0800

    给404页面增加导航栏

--- src/views/NotFound.vue
+++ src/views/NotFound.vue
@@ -1,4 +1,16 @@
 <template>
+    <div id="app">
+        <nav class="navbar">
+            <div class="nav-container">
+                <a href="#" class="logo">ZIT<span>-CoCo-Community</span></a>
+                <div class="user-info">
+                    <img :src="avatar" alt="用户头像" class="user-avatar">
+                    <div class="user-name">{{ username }}</div>
+                </div>
+            </div>
+        </nav>
+    </div>
+<div style="height: 90px;"></div>
 <div class="container">
         <div class="error-content">
             <div class="error-icon">
@@ -20,4 +32,32 @@
 
 <style>
 @import url(@/assets/style/404/style.css);
+@import url(@/assets/css/Navigation-bar.css);
 </style>
+
+<script>
+export default{
+    name:'login',
+    data(){
+        return{
+            avatar:"/images/user.png",
+            Welcome_text:"Hello,欢迎来到CoCo-Community，请使用GitHub登录",
+            username: "未登录用户" 
+        }
+    },
+    async mounted(){
+        checkLoginStatus().then((logininformation) => {
+        if (!logininformation || !logininformation.authenticated) {
+        this.username = '未登录用户';
+        this.avatar = '/images/user.png';
+        } else {
+        this.username = logininformation.user.name || logininformation.user.login;
+        this.avatar = logininformation.user.avatar_url || '/images/user.png';
+        }
+    }).catch((err) => {
+        console.error("登录检查失败：", err);
+        this.username = '登录信息检查失败';
+    });
+    }
+}
+</script>
