4d48d73066d28e298be305436601558357357ed2
oliver <oliverxiaozhen@outlook.com>
2025-10-17 00:40:57 +0800

    设置不可调整的设置选项

--- src/views/me/index.vue
+++ src/views/me/index.vue
@@ -63,6 +63,7 @@
     <!--设置-->
   <div :class="['tab-content-me', { 'active-me': activeTab === 'settings' }]" id="settings">
     <h2 class="section-title-me">设置</h2>
+    <p>暂无可设置的项目</p>
   </div>
 </div>
   
