205d28a977806885b2d1eca3432d6304c3e5dd5b
oliver <oliverxiaozhen@outlook.com>
2025-10-21 20:07:51 +0800

    修复404页面的登录检查功能

--- src/views/NotFound.vue
+++ src/views/NotFound.vue
@@ -36,28 +36,29 @@
 </style>
 
 <script>
-export default{
-    name:'login',
-    data(){
-        return{
-            avatar:"/images/user.png",
-            Welcome_text:"Hello,欢迎来到CoCo-Community，请使用GitHub登录",
-            username: "未登录用户" 
-        }
-    },
-    async mounted(){
-        checkLoginStatus().then((logininformation) => {
-        if (!logininformation || !logininformation.authenticated) {
-        this.username = '未登录用户';
-        this.avatar = '/images/user.png';
-        } else {
-        this.username = logininformation.user.name || logininformation.user.login;
-        this.avatar = logininformation.user.avatar_url || '/images/user.png';
-        }
-    }).catch((err) => {
-        console.error("登录检查失败：", err);
-        this.username = '登录信息检查失败';
-    });
+import { checkLoginStatus } from '@/script/login';
+
+export default {
+  name: '404',
+  data() {
+    return {
+      avatar:"/images/user.png",
+      username:"未登录用户",
     }
+  },
+  mounted() {
+    checkLoginStatus().then((logininformation) => {
+    if (!logininformation || !logininformation.authenticated) {
+      this.username = '未登录用户';
+      this.avatar = '/images/user.png';
+    } else {
+      this.username = logininformation.user.name || logininformation.user.login;
+      this.avatar = logininformation.user.avatar_url || '/images/user.png';
+    }
+  }).catch((err) => {
+    console.error("登录检查失败：", err);
+    this.username = '登录信息检查失败';
+  });
+  }
 }
 </script>
