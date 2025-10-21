c31b9f3c515f04a3d4fdb656b226b3fcc4927aa6
oliver <oliverxiaozhen@outlook.com>
2025-10-21 22:11:31 +0800

    给协议公示页面换上导航栏

--- src/views/agreement/index.vue
+++ src/views/agreement/index.vue
@@ -1,10 +1,16 @@
 <template>
-    <header>
-      <div class="container">
-        <h1>ZIT-CoCo-Community协议公示</h1>
-        <p>包含适用CoCo-Community的所有协议</p>
-      </div>
-    </header>
+<div id="app">
+            <nav class="navbar">
+                <div class="nav-container">
+                    <a href="#" class="logo">ZIT<span>-CoCo-Community</span></a>
+                    <div class="user-info">
+                        <img :src="avatar" alt="用户头像" class="user-avatar">
+                        <div class="user-name">{{ username }}</div>
+                    </div>
+                </div>
+        </nav>
+    </div>
+    <div style="height: 90px;"></div>
 
 <router-link :to="{ name: 'UserAgreement'}" class="file-link">
     <span class="file-list">
@@ -36,4 +42,36 @@ useHead({
     {content: '这是Zit-CoCo-Community的协议公示页面，公示了所有适用于CoCo-Community的协议及条款，这包括《用户协议》和《隐私政策》' }
   ]
 })
+</script>
+
+<style>
+@import url(@/assets/css/Navigation-bar.css);
+</style>
+
+<script>
+import { checkLoginStatus } from '@/script/login';
+
+export default {
+  name: '404',
+  data() {
+    return {
+      avatar:"/images/user.png",
+      username:"未登录用户",
+    }
+  },
+  mounted() {
+    checkLoginStatus().then((logininformation) => {
+    if (!logininformation || !logininformation.authenticated) {
+      this.username = '未登录用户';
+      this.avatar = '/images/user.png';
+    } else {
+      this.username = logininformation.user.name || logininformation.user.login;
+      this.avatar = logininformation.user.avatar_url || '/images/user.png';
+    }
+  }).catch((err) => {
+    console.error("登录检查失败：", err);
+    this.username = '登录信息检查失败';
+  });
+  }
+}
 </script>
