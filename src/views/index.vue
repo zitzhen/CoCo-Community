6df66db2f3284d2a1b2738b2e8a6c250253305d0
oliver <oliverxiaozhen@outlook.com>
2025-11-03 22:52:09 +0800

    恢复Gitlab的按钮

--- src/views/index.vue
+++ src/views/index.vue
@@ -86,13 +86,20 @@
               </svg>
             </button>
           </a>
-          <a href="https://gitee.com/hello-oliver/CoCo-Community" target="_blank">
+          <a href="https://gitee.com/zitzhen/CoCo-Community" target="_blank">
             <button class="btn">
               <svg width="40" height="40" fill="#c71d23" xmlns="http://www.w3.org/2000/svg" viewBox="0 0 24 24" id="gitee">
                 <image href="/src/assets/images/icon/gitee.svg" width="24" height="24" x="0" y="0"/>
               </svg>
             </button>
           </a>
+          <a href="https://gitlab.com/zitzhen/CoCo-Community">
+            <button class="btn bins">
+          <svg width="40" height="40" fill="#FC6D26" xmlns="http://www.w3.org/2000/svg" viewBox="0 0 24 24" id="gitlab">
+            <path d="M22.65 14.39L12 22.13 1.35 14.39a.84.84 0 0 1-.3-.94l1.22-3.78 2.44-7.51A.42.42 0 0 1 4.82 2a.43.43 0 0 1 .58 0 .42.42 0 0 1 .11.18l2.44 7.49h8.1l2.44-7.51A.42.42 0 0 1 18.6 2a.43.43 0 0 1 .58 0 .42.42 0 0 1 .11.18l2.44 7.51L23 13.45a.84.84 0 0 1-.35.94z"/>
+          </svg>
+        </button>
+        </a>
         </div>
         <br>
         <div style="margin: 0 auto;">
