e5965ae0669c9cf0b4699f8deb1e940126ab6430
oliver <oliverxiaozhen@outlook.com>
2025-11-19 21:55:36 +0800

    修复无法发出访问记录请求的问题

--- src/views/user/index.vue
+++ src/views/user/index.vue
@@ -312,6 +312,11 @@ onMounted(async () => {
   }
 
   loading.value = false
+
+  // 发送页面浏览统计请求
+  console.log("用户名"+encodeURIComponent(username));
+  const apiUrl = `https://cc.zitzhen.cn/api/pageviews_user?username=${encodeURIComponent(username)}`;
+  fetch(apiUrl, { method: 'GET' });
 })
 </script>
 
@@ -347,10 +352,4 @@ export default {
   });
   }
 }
-
-onMounted(() => {
-  // 发送页面浏览统计请求
-  const apiUrl = `https://cc.zitzhen.cn/api/pageviews_user?username=${encodeURIComponent(username)}`;
-  fetch(apiUrl, { method: 'GET' });
-})
 </script>
